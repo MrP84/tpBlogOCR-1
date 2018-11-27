789783bb182db7b2564d9d2ae12b2fd7c9d4bb99
MrP <gllmprvnzn@gmail.com>
2018-11-27 17:18:44 +0100

    Livraison finale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tp-blog';
+/* Array des posts saisis en dur*/
+  posts = [
+    {
+      title: 'Mon premier Post !!',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      loveIts: 0
+    },
+    {
+      title: 'Déjà 2 (wouhou) !!',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      loveIts: 0
+    },
+    {
+      title: 'Et 1, et 2, et... 3 posts !',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      loveIts: 0
+    }
+  ];
 }
---
+++ src/app/post/post.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss']
+})
+export class PostComponent implements OnInit {
+
+  @Input() postTitle: string;
+  @Input() postContent: string;
+  @Input() loveIts: integer;
+
+  creationDate = new Date();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+/**
+ * Récupéraion des valeurs de loveIts
+ */
+  getLoveIts() {
+    return this.loveIts
+  }
+/**
+ * ajoute 1 à loveIts lors au click sur le bouton correspondant
+ */
+  onLike() {
+    this.loveIts++;
+    return this.loveIts;
+  }
+/**
+ * retire 1 à LoveIts au click sur le bouton correspondant
+ */
+  onDislike() {
+    this.loveIts--;
+    return this.loveIts;
+  }
+
+}
